b886be8d3b0f49932b0110656a92b6e1fbb0e2d4
Joe Lucero <jlucerond@gmail.com>
2015-05-03 17:14:23 -0400

    May 3, 5:13pm
    Updated w/ Swift v Objective-C project

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -1,7 +1,5 @@
 import UIKit
 
-// making a change in something for GitHub practicing purposes
-
 /*
 
 Strings
@@ -10,10 +8,16 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
-    return cheese
+    
+    let myFavoriteCheese = cheese + " cheese"
+    
+    return myFavoriteCheese
 }
 
-let fullSentence = favoriteCheeseStringWithCheese("cheddar")
+var myFave = favoriteCheeseStringWithCheese("cheddar")
+
+let fullSentence = "my favorite cheese is " + favoriteCheeseStringWithCheese("cheddar").stringByReplacingOccurrencesOfString(" cheese", withString: "")
+
 // Make fullSentence say "My favorite cheese is cheddar."
 
 /*
@@ -22,13 +26,17 @@ Arrays & Dictionaries
 
 */
 
-let numberArray = [1, 2, 3, 4]
+var numberArray = [1, 2, 3, 4]
 // Add 5 to this array
 // WORK HERE
+numberArray.append(5)
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
+numberDictionary[5] = "five"
+
+println(numberDictionary)
 
 /*
 
@@ -39,8 +47,16 @@ Loops
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
 
+for number in 1...10{
+    println(number)
+}
+
 // Use a half-closed range loop to print 1 - 10, inclusively
 // WORK HERE
+
+for number in 1..<10{
+    println(number)
+}
 
 let worf = [
     "name": "Worf",
@@ -60,7 +76,20 @@ let characters = [worf, picard]
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
-    return []
+    var drinkArray : [String] = [""]
+    //NOTE TO MARK: I know this is a bit of a cheat, but if I don't add this element, it doesn't work. Something about not being able to append before it gets initialized. Any suggestions?
+    
+    for character in characters{
+    
+        for (key, value) in character{
+            if key == "favorite drink"{
+                drinkArray.append(value)
+            }
+        }
+    }
+
+    drinkArray.removeAtIndex(0)
+    return drinkArray
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -79,6 +108,22 @@ let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
 
+func takeAnArrayAndMakeStrings (inputArray : Array<String>) -> String{
+    var thisString = ""
+    for var i = 0; i < inputArray.count; i++ {
+        if i != inputArray.count - 1 {
+            thisString = thisString.stringByAppendingString(inputArray[i]) + ";"
+        }
+        else{
+            thisString = thisString.stringByAppendingString(inputArray[i])
+        }
+    }
+    
+    return thisString
+}
+
+var actualOutput = takeAnArrayAndMakeStrings(strings)
+
 let expectedOutput = "milk;eggs;bread;challah"
 
 /*
@@ -91,3 +136,4 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+ let cerealArraySortedAlphabetically = sorted(cerealArray)
